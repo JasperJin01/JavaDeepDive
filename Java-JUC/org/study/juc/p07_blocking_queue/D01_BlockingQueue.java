4ba62571512124efc4d15ab35c8240131c17f2f0
TimaxThu <2426069686@qq.com>
2024-11-07 00:19:57 +0800

    提交BlockingQueue代码

---
+++ Java-JUC/org/study/juc/p07_blocking_queue/D01_BlockingQueue.java
@@ -0,0 +1,40 @@
+package org.study.juc.p07_blocking_queue;
+
+import org.junit.Test;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingDeque;
+
+/**
+ * <b>阻塞队列：</b>
+ * 和普通的队列相比，阻塞队列是有长度的
+ * 当阻塞队列已满，并且继续入队时，入队操作的线程会被阻塞（等待队列有空位置才能继续入队操作）
+ * 当阻塞队列为空，并且继续出队时，出队操作的线程会被阻塞（等待队列有数据才能出队）
+ */
+public class D01_BlockingQueue {
+
+    ArrayBlockingQueue<String> arrayBlockingQueue = new ArrayBlockingQueue(3);
+    @Test
+    public void test01() {
+        System.out.println(arrayBlockingQueue.add("a"));
+        System.out.println(arrayBlockingQueue.add("b"));
+        System.out.println(arrayBlockingQueue.add("c"));
+//        System.out.println(arrayBlockingQueue.add("d")); // 队列已满仍然入队，报异常
+    }
+    @Test
+    public void test02() {
+        System.out.println(arrayBlockingQueue.add("a"));
+        System.out.println(arrayBlockingQueue.add("b"));
+        System.out.println(arrayBlockingQueue.add("c"));
+
+    }
+
+
+
+}
+
+/**
+ * NOTE idea 的一些快捷键
+ *  cmd + opt + t: Code->surround with
+ *  ctrl + h: Navigate->Hierarchy
+ */
